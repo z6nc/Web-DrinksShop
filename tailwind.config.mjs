720075ea0c0e5568f245d3487f47a79ca81ba9a9
z6nc <andresvelasquezarotinco@gmail.com>
2024-03-23 13:05:59 -0500

    add

---
+++ tailwind.config.mjs
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {
+			fontFamily: {
+				'Nunito': ['Nunito Sans', 'sans-serif'],
+				'Protest': ['Protest Strike', 'sans-serif'],
+			  },
+			 
+
+		},
+	},
+	plugins: [],
+}
