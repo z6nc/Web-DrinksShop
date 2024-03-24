a8884741d95f4e8b57874647fd33fb0d5a0ff7b7
z6nc <andresvelasquezarotinco@gmail.com>
2024-03-24 14:09:37 -0500

    add style in index and sliders

---
+++ src/sections/proveedores.ts
@@ -0,0 +1,37 @@
+export const LogoProductos = [
+    {
+      id: "coca-cola",
+      logo: "https://brandemia.org/contenido/subidas/2023/01/logo-coca-cola-1891-1941-1024x576.png",
+      url: "https://www.coca-cola.com/pe/es",
+    },
+    {
+      id: "Pilsen-Callao",
+      logo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRmQACJm6QGqP5PA3bRfhe8TnUgN121mXfEE32Z7uxNsg&s",
+      url: "https://www.pilsencallao.com.pe/",
+    },
+    {
+      id: "Agua-SanLuis",
+      logo: "https://d1yjjnpx0p53s8.cloudfront.net/styles/logo-thumbnail/s3/0017/5617/brand.gif?itok=TXplOlAY",
+      url: "https://www.coca-cola.com/pe/es/brands/san-luis",
+    },
+    {
+      id: "InkaKola",
+      logo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTfmF1zFzQ53xBNUbZ3y5JU_tmImFSp-Wj03mNLcKP69g&s",
+      url: "https://www.coca-cola.com/pe/es/brands/inca-kola",
+    },
+    {
+      id: "Cristal",
+      logo: "https://getvectorlogo.com/wp-content/uploads/2018/12/cristal-la-cerveza-de-los-peruanos-vector-logo.png",
+      url: "https://www.cristal.com.pe/",
+    },
+    {
+      id: "Agua-Cielo",
+      logo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQmaodDr7tc2iat8Cfr8qWiRWJ_7r-O2QOOJ5QYiDCxEA&s",
+      url: "https://www.ajegroup.com/marca/cielo/",
+    },
+    {
+      id: "Agua-SanMateo",
+      logo: "https://estudiocrater.com/wp-content/uploads/2017/11/SM-01-1.jpg",
+      url: "https://www.aguasanmateo.com.pe/",
+    },
+  ];
