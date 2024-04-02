bc66f32a25eb0e2fc166bc65f32b6ba20862fc77
z6nc <andresvelasquezarotinco@gmail.com>
2024-04-02 11:04:35 -0500

    Add @midudev/tailwind-animations package and update product images

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,4 +1,5 @@
 /** @type {import('tailwindcss').Config} */
+import animations from '@midudev/tailwind-animations'
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
@@ -13,5 +14,5 @@ export default {
 
 		},
 	},
-	plugins: [],
+	plugins: [animations],
 }
