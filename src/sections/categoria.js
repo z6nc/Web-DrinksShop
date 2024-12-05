daa4a6ccdd6485e4b710ed63ac5cfbe94385ae2e
z6nc <andresvelasquezarotinco@gmail.com>
2024-12-04 21:14:31 -0500

    Update category images for improved branding and consistency

--- src/sections/categoria.js
+++ src/sections/categoria.js
@@ -2,17 +2,17 @@ export const Categorias=[
     {
     id:"energizante",
     nombre:"energizantes",
-    img:"https://tse2.mm.bing.net/th?id=OIG2.lnsxpIoDn_yS5Qer3hGw&pid=ImgGn",
+    img:"https://tse3.mm.bing.net/th?id=OIG2.JHR0IxVljLAF9I5C1Yjg&pid=ImgGn",
 },
 {
     id:"nutricional",
     nombre:"nutricionales",
-    img:"https://tse3.mm.bing.net/th?id=OIG2.JHR0IxVljLAF9I5C1Yjg&pid=ImgGn",
+    img:"https://tse2.mm.bing.net/th?id=OIG2.lnsxpIoDn_yS5Qer3hGw&pid=ImgGn",
 },
 {
     id:"hidratante",
     nombre:"hidratantes",
-    img:"https://tse3.mm.bing.net/th?id=OIG3..HeHIpQA_aLS0Jr5tfEw&pid=ImgGn",
+    img:"https://tse3.mm.bing.net/th?id=OIG2.F0PkVNnlExPennPU_qQp&pid=ImgGn",
 },
 
 ]
